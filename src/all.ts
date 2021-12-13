80e19f9f21bd2e74db7f793197fe6ec0395e3785
Éverton M. Vieira <everton.muvi@gmail.com>
2021-12-13 16:38:25 -0300

    prototype development

--- src/all.ts
+++ src/all.ts
@@ -1,34 +1,34 @@
+export { QinBoolean, QinBooleanOptions } from "./qin-boolean";
+
+export { QinLabel } from "./qin-label";
+
+export { QinString, QinStringOptions } from "./qin-string";
+
+export { QinIcon } from "./qin-icon";
+
+export { QinTabs, QinTabsOptions, QinTab } from "./qin-tabs";
+
+export { QinPanel } from "./qin-panel";
+
 export { QinAsset, qinAssetUrl } from "./qin-assets";
 
-export { QinBoolean, QinBooleanOptions } from "./qin-boolean";
+export { QinField } from "./qin-field";
+
+export { QinChooser, QinChooserOptions, QinChosen } from "./qin-chooser";
+
+export { QinMutants, QinMutantsArm } from "./qin-mutants";
 
 export { QinButton, QinButtonOptions } from "./qin-button";
 
-export { QinChooser, QinChooserOptions, QinChosen } from "./qin-chooser";
+export { QinLine, QinLineOptions } from "./qin-line";
 
 export { QinColumn, QinColumnOptions } from "./qin-column";
 
 export { QinCombo, QinComboOptions, QinComboItem } from "./qin-combo";
 
-export { QinExplorer, QinExplorerOptions } from "./qin-explorer";
-
-export { QinField } from "./qin-field";
-
-export { QinIcon } from "./qin-icon";
-
-export { QinInteger, QinIntegerOptions } from "./qin-integer";
-
-export { QinLabel } from "./qin-label";
-
-export { QinLine, QinLineOptions } from "./qin-line";
-
-export { QinMutants, QinMutantsArm } from "./qin-mutants";
-
-export { QinPanel } from "./qin-panel";
-
 export { QinPath, QinPathOptions } from "./qin-path";
 
-export { QinString, QinStringOptions } from "./qin-string";
+export { QinExplorer, QinExplorerOptions } from "./qin-explorer";
 
-export { QinTabs, QinTabsOptions, QinTab } from "./qin-tabs";
+export { QinInteger, QinIntegerOptions } from "./qin-integer";
 
